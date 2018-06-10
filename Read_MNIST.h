47324832ccd930f7f0e2b3bb442b052132e5b92c
jeanette-nguyen <jeanette.nguyen.0825@gmail.com>
2018-06-10 05:00:20 -0700

    Read Mnist header

---
+++ Read_MNIST.h
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <assert.h>
+#include "kmeans.h"
+
+void cleanup(FILE*, FILE*);
+void Valid_File(int, int, FILE*, FILE*);
+static unsigned int mnist_bin_to_int(char*);
+void Read_Data(kmeans_model*, char*, char*);
